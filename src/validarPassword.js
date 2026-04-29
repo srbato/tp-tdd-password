96333be81404d3d908fa7ac2311174436c2eb477
srbato <srbattini@gmail.com>
2026-04-29 14:40:27 -0300

    refactor: extraer reglas a estructura declarativa con array de objetos

--- src/validarPassword.js
+++ src/validarPassword.js
@@ -1,27 +1,32 @@
 const LONGITUD_MINIMA = 8;
 
+const reglas = [
+    {
+        cumple: (password) => password.length >= LONGITUD_MINIMA,
+        mensajeError: 'Debe tener al menos 8 caracteres'
+    },
+    {
+        cumple: (password) => /[A-Z]/.test(password),
+        mensajeError: 'Debe contener al menos una mayuscula'
+    },
+    {
+        cumple: (password) => /[a-z]/.test(password),
+        mensajeError: 'Debe contener al menos una minuscula'
+    },
+    {
+        cumple: (password) => /[0-9]/.test(password),
+        mensajeError: 'Debe contener al menos un numero'
+    },
+    {
+        cumple: (password) => /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password),
+        mensajeError: 'Debe contener al menos un simbolo especial'
+    }
+];
+
 function validarPassword(password) {
-    const errores = [];
-
-    if (password.length < LONGITUD_MINIMA) {
-        errores.push('Debe tener al menos 8 caracteres');
-    }
-
-    if (!/[A-Z]/.test(password)) {
-        errores.push('Debe contener al menos una mayuscula');
-    }
-
-    if (!/[a-z]/.test(password)) {
-        errores.push('Debe contener al menos una minuscula');
-    }
-
-    if (!/[0-9]/.test(password)) {
-        errores.push('Debe contener al menos un numero');
-    }
-
-    if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
-        errores.push('Debe contener al menos un simbolo especial');
-    }
+    const errores = reglas
+        .filter(regla => !regla.cumple(password))
+        .map(regla => regla.mensajeError);
 
     return {
         esValida: errores.length === 0,
